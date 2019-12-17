4ba0c0f25243a151aeb9ab2788b9636cb74f61a9
yudai miyaguni <s19017@std.it-college.ac.jp>
2019-12-17 14:27:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h2>塔を立てるときは、まず座って費用を計算しよう</h2>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
