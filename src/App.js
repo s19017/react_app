05405bf09db37869a398e190381ca63dd1ff65a1
yudai miyaguni <s19017@std.it-college.ac.jp>
2019-12-17 17:06:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Hello = props => {
-    return <h1>Hello world</h1>
+const item = "SDカード"
+const value = 1200
+    return <h1>{item} - {value}円</h1>
 }
 
 class App extends React.Component {
