43d50905f51bcb712901e7a175c2031a74f15b3c
yudai miyaguni <s19017@std.it-college.ac.jp>
2019-12-17 16:32:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
-import React from 'react'
+import React from 'react'                                         
 
-const
-export default App;
+const Hello = props => {
+const r = Math.floor(Math.random() * 3)
+
+  if (r == 0) return <p>今日も頑張りましょう。</p>
+  if (r == 1) return <p>こんにちは</p>
+  if (r == 2) return <p>朗らかな日ですね。</p>
+
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render () {
+      return (
+        <div>
+            <Hello />
+        </div>
+      )
+    }
+}
+
+export default App
